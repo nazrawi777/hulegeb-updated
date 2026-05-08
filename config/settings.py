be1802cd321339a838c95a05cdb6ff860769101c
nazrawi777 <151239515+nazrawi777@users.noreply.github.com>
2026-05-08 18:53:52 +0000

    prepare render deployment settings

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9z)s5_v)ig9p66=t&i5j$&*pw5=he(j2#%mz8gy7=cgwx4de4r'
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -131,6 +133,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files
 MEDIA_URL = '/media/'
